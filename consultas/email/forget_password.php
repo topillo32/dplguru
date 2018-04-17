e5ee472deadd0bb8219cba46ec9192a17c3b42e1
Unknown <topillo32@gmail.com>
2018-04-16 23:02:32 -0300

    email

--- consultas/email/forget_password.php
+++ consultas/email/forget_password.php
@@ -10,17 +10,16 @@ $users->email = $data->email;
 if (isset($auth->email) && $auth->email != "") {
   $users->readOne();
   $tokenuser = $auth->createPassInicial();
-  $mensaje = 'Por favor haga click en el siguiente <a href="https://dplguru.com/#/changepasswd/' . $tokenuser . '/' . $users->idUser . '">link</a> para generar una clave';
+  $mensaje = 'Please click on the following <a href="https://dplguru.com/#/changepasswd/' . $tokenuser . '/' . $users->idUser . '">link</a> to generate a password';
   $email = new Email("dplgurupwd");
-  $email->asunto = "Generar clave de usuario";
+  $email->asunto = "New Password";
   $email->razonSocialRecibe = $users->userName;
   $email->razonSocialEnvia = "Dplguru";
   $email->email = $data->email;
   $email->mensaje = $mensaje;
   $email->enviar();
   echo json_encode(array(
-    "code" => 0,
-    "donde estoy" => "de forget a email",
+    "code" => 0    
   ));
 } else {
   echo json_encode(array(
