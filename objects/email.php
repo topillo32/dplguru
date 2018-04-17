c5bd4c3ff9a3f4abcf86fce036b559f509543443
Unknown <topillo32@gmail.com>
2018-04-16 22:27:13 -0300

    commit

--- objects/email.php
+++ objects/email.php
@@ -1,7 +1,7 @@
 <?php
 use PHPMailer/PHPMailer/PHPMailer;
 
-require_once  './../vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 class Email {
   protected $server;
   protected $username;
