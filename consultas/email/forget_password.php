40c72506ee41631a9cf977b71da38347513658dc
Unknown <topillo32@gmail.com>
2018-04-16 21:45:17 -0300

    commit

--- consultas/email/forget_password.php
+++ consultas/email/forget_password.php
@@ -4,27 +4,26 @@ $database = new Database();
 $db = $database->getConnection();
 $auth = new Auth($db);
 $users = new Usuarios($db);
-$email = new Email();
 $data = json_decode(file_get_contents("php://input"));
 $auth->email = $data->email;
-$users->email = $data->email;/*
+$users->email = $data->email;
 if (isset($auth->email) && $auth->email != "") {
-  	$users->readOne();
-	$tokenuser = $auth->createPassInicial();
-	$mensaje = 'Please click on the following <a href="https://dplguru.com/#/changepasswd/' . $tokenuser . '/' . $users->idUser . '">link</a> to generate a password';
-	$email->asunto = "Generar clave de usuario";
-	$email->razonSocialRecibe = $users->userName;
-	$email->razonSocialEnvia = "Dplguru";
-	$email->email = $data->email;
-	$email->mensaje = $mensaje;
-	$email->enviar();
-	echo json_encode(array(
-		"code" => 0,
-	));
-
+  $users->readOne();
+  $tokenuser = $auth->createPassInicial();
+  $mensaje = 'Por favor haga click en el siguiente <a href="https://dplguru.com/#/changepasswd/' . $tokenuser . '/' . $users->idUser . '">link</a> para generar una clave';
+  $email = new Email("dplgurupwd");
+  $email->asunto = "Generar clave de usuario";
+  $email->razonSocialRecibe = $users->userName;
+  $email->razonSocialEnvia = "Dplguru";
+  $email->email = $data->email;
+  $email->mensaje = $mensaje;
+  $email->enviar();
+  echo json_encode(array(
+    "code" => 0,
+    "donde estoy" => "de forget a email",
+  ));
 } else {
-	echo json_encode(array(
-		"code" => 1,
-	));
+  echo json_encode(array(
+    "code" => 1,
+));
 }
-*/
--- objects/email.php
+++ objects/email.php
@@ -1,13 +1,12 @@
 <?php
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\Exception;
-//
-// require_once dirname(__DIR__) . '\vendor\autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require_once dirname(__DIR__) . '\vendor\autoload.php';
 
 class Email {
-	protected $server;
-	protected $username;
-	protected $password;
+  protected $server;
+  protected $username;
+  protected $password;
 	public $email;
 	public $razonSocialEnvia;
 	public $razonSocialRecibe;
@@ -15,45 +14,43 @@ class Email {
 	public $asunto;
 	public $mensaje;
 
-	public function __construct() {
-		// $this->username = 'dplgurupwd@gmail.com';
-		// $this->password = 'dplguru1234';
-		// $this->server = '{170.239.86.173:993/imap/ssl/novalidate-cert}INBOX';
+	public function __construct($alias) {
+		$this->username = $alias . '@gmail.com';
+		$this->password = 'dplguru1234';
+		$this->server = '{170.239.86.173:993/imap/ssl/novalidate-cert}INBOX';
 	}
 
-  public function enviar() {
-		echo "Entra" . $this->username . "  ":
-/*
-		$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+	public function enviar() {
 		try {
-		    $mail->SMTPDebug = 1;                                 // Enable verbose debug output
-		    $mail->isSMTP();                                      // Set mailer to use SMTP
-				$mail->SMTPOptions = array(
-						'ssl' => array(
-						'verify_peer' => false,
-						'verify_peer_name' => false,
-						'allow_self_signed' => true
-						)
-				);
-		    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-		    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-		    $mail->Username = $this->username;                 // SMTP username
-		    $mail->Password = $this->password;                           // SMTP password
-		    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-		    $mail->Port = 587;                                    // TCP port to connect to
-		    //Recipients
-		    $mail->setFrom($this->username, 'DPLGURU');
-		    $mail->addAddress($this->email, $this->razonSocialRecibe);     // Add a recipient
-		    $mail->isHTML(true);                                  // Set email format to HTML
-		    $mail->Subject = $this->asunto;
-		    $mail->Body    = $this->mensaje;
-		    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+		$mail->SMTPDebug = 1;                                 // Enable verbose debug output
+		$mail->isSMTP();                                      // Set mailer to use SMTP
+		$mail->SMTPOptions = array(
+				'ssl' => array(
+				'verify_peer' => false,
+				'verify_peer_name' => false,
+				'allow_self_signed' => true
+				)
+		);
+		$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+		$mail->SMTPAuth = true;                               // Enable SMTP authentication
+		$mail->Username = $this->username;                 // SMTP username
+		$mail->Password = $this->password;                           // SMTP password
+		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+		$mail->Port = 587;                                    // TCP port to connect to
+		//Recipients
+		$mail->setFrom($this->username, 'DPLGURU');
+		$mail->addAddress($this->email, $this->razonSocialRecibe);     // Add a recipient
+		$mail->isHTML(true);                                  // Set email format to HTML
+		$mail->Subject = $this->asunto;
+		$mail->Body    = $this->mensaje;
+		// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-		    $mail->send();
-		    echo 'Message has been sent';
+		$mail->send();
+		echo 'Message has been sent';
 		} catch (Exception $e) {
-		    echo 'Message could not be sent.';
-		    echo 'Mailer Error: ' . $mail->ErrorInfo;
+				echo 'Message could not be sent.';
+				echo 'Mailer Error: ' . $mail->ErrorInfo;
 		}*/
-  }
+
+	}
 }
